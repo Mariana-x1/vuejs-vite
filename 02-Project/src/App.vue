969fad2a26b0cda20cfe163714b147897232f445
Mari Astamadze <marihamburg321@gmail.com>
2024-04-13 21:21:47 +0200

    reactive update erzeugen

--- 02-Project/src/App.vue
+++ 02-Project/src/App.vue
@@ -1,3 +1,13 @@
 <template>
+  <HelloWorld />
+  <br />
   <h1>Hej Vue Vite</h1>
 </template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld }
+}
+</script>
---
+++ 02-Project/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+let x = ref(10)
+
+const decrease = () => {
+  x.value -= 1
+}
+const increase = () => {
+  x.value += 1
+}
+</script>
+
+<template>
+  <div>
+    <h2>Hallo welt</h2>
+    <button v-on:click="decrease">Update --</button>
+    <p>{{ x / 2 }}</p>
+    <button v-on:click="increase">Update ++</button>
+  </div>
+</template>
